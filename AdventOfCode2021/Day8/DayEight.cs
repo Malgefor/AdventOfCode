e302140091a0a2e9b8a9c9ea448de9b92bc2aa32
Vincent Rutten <vincent@vicompany.nl>
2021-12-20 20:47:42 +0100

    Add Day8 and Day9 code

---
+++ AdventOfCode2021/Day8/DayEight.cs
@@ -0,0 +1,178 @@
+﻿using LanguageExt;
+
+namespace AdventOfCode2021.Day8;
+
+public class DayEight : IPuzzleDay
+{
+    public IEnumerable<PuzzleResult> PuzzleResults()
+    {
+        var inputLines = GetParsedInput();
+
+        var result = inputLines
+            .Bind(
+                line => line
+                    .OutputValues
+                    .Filter(
+                        outputValue => IsOne(outputValue) || IsFour(outputValue) || IsSeven(outputValue) || IsEight(outputValue)))
+            .Count;
+
+        yield return new PuzzleResult(1, result);
+
+        var sumOfOutputValues = inputLines.Map(
+            line =>
+            {
+                var oneValue = line.UniqueCombinations.First(IsOne).ToSeq();
+                var fourValue = line.UniqueCombinations.First(IsFour).ToSeq();
+                var sevenValue = line.UniqueCombinations.First(IsSeven).ToSeq();
+                var eightValue = line.UniqueCombinations.First(IsEight).ToSeq();
+
+                var sixValue = line.UniqueCombinations
+                    .Filter(combination => combination.Length == 6)
+                    .Filter(combination => combination.Intersect(oneValue).ToSeq().Length == 1)
+                    .First();
+
+                var topValue = sevenValue.Except(oneValue).First();
+                var bottomRight = sixValue.Intersect(oneValue).First();
+                var topRight = oneValue.Except(Prelude.Seq1(bottomRight)).First();
+
+                var threeValue = line.UniqueCombinations
+                    .Filter(value => value.Length == 5)
+                    .Filter(value => value.Contains(topValue) && value.Contains(bottomRight) && value.Contains(topRight))
+                    .First();
+
+                var topLeft = fourValue.Except(threeValue.ToSeq()).First();
+                var middleValue = fourValue.Except(Seq.create(topLeft, topRight, bottomRight)).First();
+                var bottomValue = threeValue.Except(Seq.create(topValue, topRight, middleValue, bottomRight)).First();
+                var bottomLeft =
+                    eightValue
+                        .Except(Seq.create(topValue, topLeft, topRight, middleValue, bottomRight, bottomValue))
+                        .First();
+
+                var displayNumber = new DisplayNumber(
+                    topValue,
+                    middleValue,
+                    bottomValue,
+                    topLeft,
+                    topRight,
+                    bottomLeft,
+                    bottomRight);
+
+                return line.OutputValues
+                    .Map(value => displayNumber.CreateDigit(value))
+                    .Reduce(ConcatIntegers);
+            });
+
+        yield return new PuzzleResult(2, sumOfOutputValues.Sum(number => number));
+    }
+
+    private static bool IsOne(string value) => value.Length == 2;
+
+    private static bool IsFour(string value) => value.Length == 4;
+
+    private static bool IsSeven(string value) => value.Length == 3;
+
+    private static bool IsEight(string value) => value.Length == 7;
+
+    private static Seq<InputLine> GetParsedInput() => FileProvider
+        .GetAllLines("Day8.input.txt")
+        .Map(
+            inputString =>
+            {
+                var splitSegments = inputString.Split('|');
+                var uniqueCombinations = splitSegments.First().Trim().Split(" ").ToSeq();
+                var outputValues = splitSegments.Last().Trim().Split(" ").ToSeq();
+
+                return new InputLine(uniqueCombinations, outputValues);
+            })
+        .ToSeq();
+
+    private record InputLine(Seq<string> UniqueCombinations, Seq<string> OutputValues);
+
+    private record DisplayNumber(
+        char Top,
+        char Middle,
+        char Bottom,
+        char TopLeft,
+        char TopRight,
+        char BottomLeft,
+        char BottomRight)
+    {
+        public int CreateDigit(string input)
+        {
+            if (IsOne(input))
+            {
+                return 1;
+            }
+
+            if (IsFour(input))
+            {
+                return 4;
+            }
+
+            if (IsSeven(input))
+            {
+                return 7;
+            }
+
+            if (IsEight(input))
+            {
+                return 8;
+            }
+
+            if (input.Contains(this.Top)
+                && input.Contains(this.TopLeft)
+                && input.Contains(this.TopRight)
+                && input.Contains(this.BottomRight)
+                && input.Contains(this.BottomLeft)
+                && input.Contains(this.Bottom))
+            {
+                return 0;
+            }
+
+            if (input.Contains(this.Top)
+                && input.Contains(this.TopLeft)
+                && input.Contains(this.Middle)
+                && input.Contains(this.BottomRight)
+                && input.Contains(this.BottomLeft)
+                && input.Contains(this.Bottom))
+            {
+                return 6;
+            }
+
+            if (input.Contains(this.Top)
+                && input.Contains(this.TopLeft)
+                && input.Contains(this.TopRight)
+                && input.Contains(this.BottomRight)
+                && input.Contains(this.Middle)
+                && input.Contains(this.Bottom))
+            {
+                return 9;
+            }
+
+            if (input.Contains(this.Top)
+                && input.Contains(this.TopRight)
+                && input.Contains(this.Middle)
+                && input.Contains(this.BottomLeft)
+                && input.Contains(this.Bottom))
+            {
+                return 2;
+            }
+
+            if (input.Contains(this.Top)
+                && input.Contains(this.TopRight)
+                && input.Contains(this.Middle)
+                && input.Contains(this.BottomRight)
+                && input.Contains(this.Bottom))
+            {
+                return 3;
+            }
+
+            return 5;
+        }
+    }
+
+    private static int ConcatIntegers(int a, int b)
+    {
+        return int.Parse(a.ToString() + b);
+    }
+}
---
+++ AdventOfCode2021/Day9/DayNine.cs
@@ -0,0 +1,148 @@
+﻿using LanguageExt;
+
+namespace AdventOfCode2021.Day9;
+
+public class DayNine : IPuzzleDay
+{
+    public IEnumerable<PuzzleResult> PuzzleResults()
+    {
+        var input = GetParsedInput()
+            .Filter(position => position.Value != 9);
+
+        var lowPoints = input.Filter(
+            position => FlattenOptionsToSequence(
+                    FindLeftOfPosition(input, position),
+                    FindRightOfPosition(input, position),
+                    FindAboveOfPosition(input, position),
+                    FindBelowOfPosition(input, position))
+                .Memo()
+                .All(adjacentPosition => adjacentPosition.Value > position.Value));
+
+        var riskScore = lowPoints.Sum(position => position.Value + 1);
+        yield return new PuzzleResult(1, riskScore);
+
+        var productOfLargestBasins = lowPoints
+            .Aggregate<Position, (Seq<Position>, Seq<int>), Seq<int>>(
+                (input, Seq.empty<int>()),
+                ((Seq<Position> input, Seq<int> basinScores) accumulatedValues, Position lowPoint) =>
+                {
+                    var basinResult = Dfs(lowPoint, accumulatedValues.input);
+                    return (basinResult.Input, accumulatedValues.basinScores.Add(basinResult.Value));
+                },
+                result => result.Item2)
+            .OrderByDescending(basinScore => basinScore)
+            .Take(3)
+            .Reduce((reducer, nextValue) => reducer * nextValue);
+
+        yield return new PuzzleResult(2, productOfLargestBasins);
+    }
+
+    private static Option<Position> FindBelowOfPosition(Seq<Position> input, Position position) => input.Find(
+        inputPosition => inputPosition.XPosition == position.XPosition + 1
+                         && inputPosition.YPosition == position.YPosition);
+
+    private static Option<Position> FindAboveOfPosition(Seq<Position> input, Position position) => input.Find(
+        inputPosition => inputPosition.XPosition == position.XPosition - 1
+                         && inputPosition.YPosition == position.YPosition);
+
+    private static Option<Position> FindRightOfPosition(Seq<Position> input, Position position) => input.Find(
+        inputPosition => inputPosition.YPosition == position.YPosition + 1
+                         && inputPosition.XPosition == position.XPosition);
+
+    private static Option<Position> FindLeftOfPosition(Seq<Position> input, Position position) => input.Find(
+        inputPosition => inputPosition.YPosition == position.YPosition - 1
+                         && inputPosition.XPosition == position.XPosition);
+
+    private static (Position CurrentPosition, int Value, Seq<Position> Input) Dfs(
+        Position currentPosition,
+        Seq<Position> input)
+    {
+        var leftoverPositionsWithoutCurrent = input.Except(Prelude.Seq1(currentPosition)).ToSeq();
+
+        var leftOfPosition = TryExplore(
+            tobeExpoloredPositions => FindLeftOfPosition(tobeExpoloredPositions, currentPosition),
+            leftoverPositionsWithoutCurrent);
+
+        var setOfUncheckedPositionsAfterLeft = leftOfPosition
+            .Match(
+                tuple => tuple.Input,
+                () => leftoverPositionsWithoutCurrent);
+
+        var rightOfPosition = TryExplore(
+            tobeExpoloredPositions => FindRightOfPosition(tobeExpoloredPositions, currentPosition),
+            setOfUncheckedPositionsAfterLeft);
+
+        var setOfUncheckedPositionsAfterRight = rightOfPosition
+            .Match(
+                tuple => tuple.Input,
+                () => setOfUncheckedPositionsAfterLeft);
+
+        var aboveOfPosition = TryExplore(
+            tobeExpoloredPositions => FindAboveOfPosition(tobeExpoloredPositions, currentPosition),
+            setOfUncheckedPositionsAfterRight);
+
+        var setOfUncheckedPositionsAfterAbove = aboveOfPosition
+            .Match(
+                tuple => tuple.Input,
+                () => setOfUncheckedPositionsAfterRight);
+
+        var belowOfPosition = TryExplore(
+            tobeExpoloredPositions => FindBelowOfPosition(tobeExpoloredPositions, currentPosition),
+            setOfUncheckedPositionsAfterAbove);
+
+        var setOfUncheckedPositionsAfterBelow = belowOfPosition.Match(
+            tuple => tuple.Input,
+            () => setOfUncheckedPositionsAfterAbove);
+
+        var currentPositionValue = FlattenOptionsToSequence(
+                leftOfPosition,
+                rightOfPosition,
+                belowOfPosition,
+                aboveOfPosition)
+            .Sum(tuple => tuple.Item2);
+
+        return (currentPosition, currentPositionValue + 1, setOfUncheckedPositionsAfterBelow);
+    }
+
+    private static Seq<Position> GetParsedInput()
+    {
+        return FileProvider
+            .GetAllLines("Day9.input.txt")
+            .Map(
+                (rowIndex, lineOfPosition) =>
+                {
+                    return lineOfPosition
+                        .Map(
+                            (columnIndex, valueOfPosition) => new Position(
+                                rowIndex,
+                                columnIndex,
+                                int.Parse(valueOfPosition.ToString())))
+                        .ToSeq();
+                })
+            .Bind(seq => seq)
+            .ToSeq();
+    }
+
+    private static Option<(Position CurrentPosition, int Value, Seq<Position> Input)> TryExplore(
+        Func<Seq<Position>, Option<Position>> possiblePosition,
+        Seq<Position> toBeExploredPositions)
+    {
+        return possiblePosition(toBeExploredPositions)
+            .Map(
+                position =>
+                {
+                    var valueTuple = Dfs(position, toBeExploredPositions);
+                    return (valueTuple.CurrentPosition, valueTuple.Value, valueTuple.Input);
+                });
+    }
+
+    private record Position(int XPosition, int YPosition, int Value);
+
+    private static Seq<T> FlattenOptionsToSequence<T>(params Option<T>[] values)
+    {
+        return Seq
+            .create(values)
+            .Bind(option => option)
+            .ToSeq();
+    }
+}
