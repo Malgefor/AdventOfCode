f1421bad7b8c590eb208d10a2fe4a8f51768b8d0
Vincent Rutten <malgefor1995@gmail.com>
2023-12-07 08:06:45 +0100

    [WIP] - Day5 parsing

---
+++ AdventOfCode2023/Day5/DayFive.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.RegularExpressions;
+
+using AdventOfCode.Generic;
+
+using LanguageExt;
+
+namespace AdventOfCode2023.Day5;
+
+public class DayFive : IPuzzleDay
+{
+    public int DayNumber => 5;
+
+    public IEnumerable<PuzzleResult> PuzzleResults()
+    {
+        var parsedInput = GetParsedInput();
+        yield return new PuzzleResult(1, 1);
+    }
+
+    private record AlmanacRangesRanges(int DestinationRangeStart, int SourceRangeStart, int RangeLength);
+
+    private static (Seq<int> Seeds, Seq<Seq<AlmanacRangesRanges>> AlmanacEntries) GetParsedInput()
+    {
+        const string pattern = @"\b\d+\b";
+
+        var allLines = FileProvider.GetAllLines("Day5.input.txt", "\r\n\r\n");
+        var seeds = Regex.Matches(allLines.Head, pattern).ToSeq().Map(x => int.Parse(x.Value));
+
+        return (seeds, allLines.Tail.Map(ParseAlmanacEntries));
+    }
+
+    private static Seq<AlmanacRangesRanges> ParseAlmanacEntries(string groupOfLines) => groupOfLines
+        .Split("\r\n")
+        .Tail()
+        .Map(line => line.Split(' '))
+        .Map(
+            splitLine => new AlmanacRangesRanges(
+                int.Parse(splitLine[0]),
+                int.Parse(splitLine[1]),
+                int.Parse(splitLine[2])))
+        .ToSeq();
+}
