96cd6e586b7ee1edeff14e36700c6ecb8c8f7c88
Vincent Rutten <vincent@vicompany.nl>
2024-12-07 10:04:35 +0100

    Day5 part 1

--- AdventOfCode2024/Day5/DayFive.cs
+++ AdventOfCode2024/Day5/DayFive.cs
@@ -10,10 +10,23 @@ public class DayFive : IPuzzleDay
         var orderingRules = parsedData.Head
             .Split("\r\n")
             .Map(x => x.Split("|"))
-            .Map(x => new OrderingRule(int.Parse(x[0]), int.Parse(x[1])));
-        var updates = parsedData.Tail[0].Split("\r\n");
+            .Map(x => new OrderingRule(int.Parse(x[0]), int.Parse(x[1])))
+            .ToSeq();
 
-        yield return new PuzzleResult(1);
+        var updates = parsedData.Tail[0]
+            .Split("\r\n")
+            .Map(y => y.Split(',').Map(int.Parse).ToArray())
+            .ToSeq();
+
+        var middleValues =
+            from update in updates
+            let isCorrect = orderingRules
+                .Filter(x => update.Contains(x.First) && update.Contains(x.Second))
+                .All(x => Array.IndexOf(update, x.First) < Array.IndexOf(update, x.Second))
+            where isCorrect
+            select update[update.Length / 2];
+
+        yield return new PuzzleResult(middleValues.Sum());
     }
 
     private record OrderingRule(int First, int Second);
